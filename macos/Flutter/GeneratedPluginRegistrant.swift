7f737fb3155921a85ec7b8da26f703f3e7cd865f
Tosin Folarin <folarintosin2007@gmail.com>
2026-06-09 19:46:04 +0100

    add bonsoir, file_picker, flutter_local_notifications, fluttertoast, path_provider, permission_handler, and shared_preferences to pubspec.yaml dependencies

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import bonsoir_darwin
+import device_info_plus
+import file_picker
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  SwiftBonsoirPlugin.register(with: registry.registrar(forPlugin: "SwiftBonsoirPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
