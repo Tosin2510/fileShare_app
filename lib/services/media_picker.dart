975877c4bd272d8468fff2fa6a365071edb85621
Tosin Folarin <folarintosin2007@gmail.com>
2026-06-27 21:37:15 +0100

    Add media picking service

---
+++ lib/services/media_picker.dart
@@ -0,0 +1,28 @@
+import 'package:file_share_app/services/permission_service.dart';
+import 'package:flutter/material.dart';
+import 'package:wechat_assets_picker/wechat_assets_picker.dart';
+
+class MediaPickerService{
+  final PermissionService _permissionService = PermissionService();
+  Future<List<AssetEntity>?> pickMediaFiles(BuildContext context) async { // The future can return null or AssetEntity
+  try{
+    final bool hasAccess = await _permissionService.requestMediaPermission();
+    if(!hasAccess) return null;
+    if(!context.mounted) return null;
+    final List<AssetEntity>? result = await AssetPicker.pickAssets(
+      context,         
+      pickerConfig: AssetPickerConfig(
+        maxAssets: 500, //Sets file picking limit to 500 
+        requestType: RequestType.common, //For images and videos
+        textDelegate: EnglishAssetPickerTextDelegate(),
+        themeColor: Color(0xFF258CF4)
+      )
+    );
+    if(!context.mounted ) return null;
+    return result;
+  } catch(e) {
+    debugPrint("Error inside Media Picker Service $e");
+    return null;
+  }
+  }
+}
