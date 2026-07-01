731a983e166d258f7f9ab6d8487c5d71a7638128
Tosin Folarin <folarintosin2007@gmail.com>
2026-07-01 23:24:31 +0100

    add descriptive documentation comments to ApkPathService"

--- lib/services/apk_path_service.dart
+++ lib/services/apk_path_service.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 class ApkPathService {
+  // Creates the flutter side of the communication.
   static const _channel = MethodChannel('com.example.file_share_app/apk_path');
   static Future<String?> getApkPath(String packageName) async {
     try{
+      // This sends a message to the native side.
       final String? path = await _channel.invokeMethod('getApkPath', {
       'packageName': packageName,
     });
