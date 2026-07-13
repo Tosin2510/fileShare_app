3764a6da9d014e26087d33d2b16b446a96c00e7c
Tosin Folarin <folarintosin2007@gmail.com>
2026-07-13 13:22:26 +0100

    refactor main entry to use approot for network service initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:file_share_app/features/device_discovery/services/device_name_service.dart';
+import 'package:file_share_app/features/device_discovery/services/network_broadcasting.dart';
+import 'package:file_share_app/features/file_transfer/services/http_server_service.dart';
 import 'package:file_share_app/views/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart'; // This defines DeviceOrientation
@@ -24,7 +27,37 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: const FileShareHome(),
+      home: const AppRoot(),
     );
+  }
+}
+class AppRoot extends StatefulWidget{
+  const AppRoot({super.key});
+  @override
+  State<AppRoot> createState() => _AppRootState();
+}
+class _AppRootState extends State<AppRoot> {
+  final NetworkBroadcasting _broadcasting = NetworkBroadcasting();
+  final ReceiveServer _receiveServer = ReceiveServer.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    _initBroadcasting();
+    _receiveServer.start();
+  }
+  @override
+  void dispose() {
+    _broadcasting.dispose();
+    _receiveServer.dispose();
+    super.dispose();
+  }
+  Future<void> _initBroadcasting() async {
+    final deviceName = await DeviceNameService.getDeviceName();
+    await _broadcasting.startBroadcasting(deviceName: deviceName);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return const FileShareHome();
   }
 }
