21fa64d9bb0bbc3be2837be3686d0b4ba91a486e
Tosin Folarin <folarintosin2007@gmail.com>
2026-07-13 13:26:56 +0100

    add DeviceNameService for local device naming

---
+++ lib/features/device_discovery/services/device_name_service.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+import 'dart:math';
+
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+class DeviceNameService{
+  static const _suffixKey = 'device_suffix';
+  static Future<String> getDeviceName() async {
+    final baseName = await _getBaseName();
+    final suffix = await _getOrCreateSuffix();
+    return '$baseName ($suffix)';
+  }
+  static Future<String> _getBaseName() async {
+    final deviceInfo = DeviceInfoPlugin();
+    try{
+      if (Platform.isAndroid) {
+        final info = await deviceInfo.androidInfo;
+        return info.model;
+      } else if (Platform.isIOS) {
+        final info = await deviceInfo.iosInfo;
+        return info.name;
+      }
+    } catch(e) {
+      // Fall back option
+    }
+    return 'Unknown Device';
+  }
+  static Future<String> _getOrCreateSuffix() async {
+    final prefs = await SharedPreferences.getInstance();
+    String? suffix = prefs.getString(_suffixKey);
+    if (suffix == null) {
+      suffix = _generateSuffix();
+      await prefs.setString(_suffixKey, suffix);
+    }
+    return suffix;
+  }
+  static String _generateSuffix() {
+    const character = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    final rand = Random();
+    return List.generate(4, (_) => character[rand.nextInt(character.length)]).join();
+  }
+}
