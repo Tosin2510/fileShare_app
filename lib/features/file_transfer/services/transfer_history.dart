23a132acf6a5d5f9233f4ec0995ba97eaf9cd40f
Tosin Folarin <folarintosin2007@gmail.com>
2026-07-30 19:43:38 +0100

    Add transfer history service

---
+++ lib/features/file_transfer/services/transfer_history.dart
@@ -0,0 +1,65 @@
+import 'package:file_share_app/features/file_transfer/models/transfer_item.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class TransferHistoryService {
+  TransferHistoryService._internal();
+  static final TransferHistoryService instance = TransferHistoryService._internal();
+  Database? _database;
+  Future<Database> get _db async {
+    if (_database != null) return _database!;
+    final path = join(await getDatabasesPath(), 'file_transfer_history.db');
+    _database = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+            await db.execute(
+              '''
+              Create table file_transfer_history(
+              id TEXT PRIMARY KEY,
+              fileName TEXT,
+              mimeType TEXT,
+              totalBytes INTEGER,
+              transferDirection TEXT,
+              transferStatus TEXT,
+              savedAt TEXT,
+              timeStamp INTEGER)
+              '''
+            );
+          },
+        );
+        return _database!;
+      }
+
+      Future<void> recordSharing(TransferItem item) async {
+        final db = await _db;
+        await db.insert(
+          'file_transfer_history',
+          {
+            'id': item.id,
+            'fileName': item.fileName,
+            'mimeType' : item.mimeType,
+            'totalBytes' : item.totalBytes,
+            'transferDirection': item.direction.name,
+            'transferStatus': item.status.name,
+            'savedAt' : item.savedPath,
+            'timeStamp' : DateTime.now().millisecondsSinceEpoch,
+          },
+          conflictAlgorithm : ConflictAlgorithm.replace,
+        );
+      }
+
+      Future<List<Map<String, dynamic>>> getAllTransferHistory() async {
+        final db = await _db;
+        return db.query('file_transfer_history', orderBy: 'timeStamp DESC');
+      }
+
+      Future<void> deleteTransferHistory(List<String> ids) async {
+        final db = await _db;
+        await db.delete(
+          'file_transfer_history',
+          where: 'id IN (${ids.map((_) => '?').join(',')})',
+          whereArgs: ids,
+        );
+      }
+}
