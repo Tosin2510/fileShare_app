b7f7ad167fe3fdbe73110910de329862e27a10dc
Tosin Folarin <folarintosin2007@gmail.com>
2026-07-31 17:07:34 +0100

    Add transfer tile model

---
+++ lib/features/file_transfer/models/transfer_tile.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class TransferTile {
+  final String id;
+  final String fileName;
+  final IconData icon;
+  final String sizeLabel;
+  final String? statusLabel;
+  final Color? statusColor;
+  final Widget trailing;
+
+  const TransferTile({
+    required this.id,
+    required this.fileName,
+    required this.icon,
+    required this.sizeLabel,
+    this.statusLabel,
+    this.statusColor,
+    required this.trailing,
+  });
+}
